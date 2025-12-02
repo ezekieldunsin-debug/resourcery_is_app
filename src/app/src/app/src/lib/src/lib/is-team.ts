db3d040623c0263a6716f8643feaa8dc1fb84568
ezekieldunsin-debug <ezekieldunsin@gmail.com>
2025-12-02 17:08:25 +0100

    Delete src/app directory

--- src/app/src/app/src/lib/src/lib/is-team.ts
+++
@@ -1,7 +0,0 @@
-export const IS_TEAM_EMAILS = [
-  'is@resourcery.com',  // Add your real IS emails here
-  'ezekiela@resourcery.com.ng',
-  'malikm@resourcery.com.ng',
-].map(email => email.toLowerCase())
-
-export const isISUser = (email: string) => IS_TEAM_EMAILS.includes(email.toLowerCase())
